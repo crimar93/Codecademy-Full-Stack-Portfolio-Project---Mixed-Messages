dd9ed45beae64aeea31eb1cdeaddda6bb1fa11be
crimar93 <caroline.rimar@gmail.com>
2025-02-08 21:46:35 -0500

    Import words arrays from words.js

--- mixed_messages.js
+++ mixed_messages.js
@@ -4,5 +4,12 @@ Portfolio Project: Mixed Messages
 Goal: Generate randomized messages with at least 3 unique components
 */
 
+//Imports
+const { verbs, nouns, adjectives, adverbs } = require('./words'); //Import word lists
+
+//Functions
+
+
+
 //Test code
-console.log("Hello, world!");
+console.log(verbs[0]);
