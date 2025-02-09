4fa3306cef8c833395782e11a17c824fcfd365e8
crimar93 <caroline.rimar@gmail.com>
2025-02-08 23:00:28 -0500

    Add import/export commands for stories

--- stories.js
+++ stories.js
@@ -5,10 +5,17 @@
 const toKillAMockingbird =
   "'I'd rather you shot at {NOUN}s in the backyard, but I know you'll go after {NOUN}s. Shoot all the {NOUN}s you want, if you can hit 'em, but remember it's a sin to {VERB} a {NOUN}.' - Harper Lee, To Kill a Mockingbird";
 
-const mrRosewater = "'Hello {NOUN}. Welcome to {NOUN}. It’s {ADJECTIVE} in the summer and {ADJECTIVE} in the winter. It’s round and wet and crowded. On the outside, babies, you’ve got a hundred years here. There’s only one rule that I know of, babies-“God damn it, you’ve got to be {ADJECTIVE}.' - Kurt Vonnegut, God Bless You, Mr. Rosewater";
+const mrRosewater =
+  "'Hello {NOUN}. Welcome to {NOUN}. It’s {ADJECTIVE} in the summer and {ADJECTIVE} in the winter. It’s round and wet and crowded. On the outside, babies, you’ve got a hundred years here. There’s only one rule that I know of, babies-“God damn it, you’ve got to be {ADJECTIVE}.' - Kurt Vonnegut, God Bless You, Mr. Rosewater";
 
-const onTheRoad = "'The only {NOUN}s for me are the {ADJECTIVE} ones, the ones who are mad to {VERB}, mad to {VERB}, mad to {VERB}, desirous of everything at the same time, the ones who never yawn or say a {ADJECTIVE} thing, but burn, burn, burn like fabulous yellow {NOUN}s exploding like {NOUN}s across the stars.' - Jack Kerouac, On the Road";
+const onTheRoad =
+  "'The only {NOUN}s for me are the {ADJECTIVE} ones, the ones who are mad to {VERB}, mad to {VERB}, mad to {VERB}, desirous of everything at the same time, the ones who never yawn or say a {ADJECTIVE} thing, but burn, burn, burn like fabulous yellow {NOUN}s exploding like {NOUN}s across the stars.' - Jack Kerouac, On the Road";
 
-const chronologyOfWater = "'I don’t have any problem understanding why {NOUN} flunk out of college or {VERB} their {NOUN} or cheat on each other or break the {NOUN} or spray-paint walls. A little bit outside of things is where some people feel each other. We do it to {VERB} the frame of {NOUN}. We do it to {VERB} and {VERB} our origins in their own images. To say, I too was here.' - Lidia Yuknavitch, The Chronology of Water";
+const chronologyOfWater =
+  "'I don’t have any problem understanding why {NOUN} flunk out of college or {VERB} their {NOUN} or cheat on each other or break the {NOUN} or spray-paint walls. A little bit outside of things is where some people feel each other. We do it to {VERB} the frame of {NOUN}. We do it to {VERB} and {VERB} our origins in their own images. To say, I too was here.' - Lidia Yuknavitch, The Chronology of Water";
 
-const dune = "'I must not {VERB}. {VERB} is the mind-killer. {VERB} is the little-death that brings total obliteration. I will face my {NOUN}. I will permit it to {VERB} over me and through me. And when it has gone past I will turn the inner {NOUN} to see its {NOUN}. Where the {NOUN} has gone there will be nothing. Only I will remain.' - Frank Herbert, Dune";
+const dune =
+  "'I must not {VERB}. {VERB} is the mind-killer. {VERB} is the little-death that brings total obliteration. I will face my {NOUN}. I will permit it to {VERB} over me and through me. And when it has gone past I will turn the inner {NOUN} to see its {NOUN}. Where the {NOUN} has gone there will be nothing. Only I will remain.' - Frank Herbert, Dune";
+
+  // Export the arrays for use in other files
+  module.exports = { toKillAMockingbird, mrRosewater, onTheRoad, chronologyOfWater, dune };
