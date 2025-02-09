e3539ac86916ad9093bd9443cc1d3b4ab60034e5
crimar93 <caroline.rimar@gmail.com>
2025-02-08 19:48:39 -0500

    Added test code

--- mixed_messages.js
+++ mixed_messages.js
@@ -3,3 +3,6 @@ Codecademy Full-Stack Program
 Portfolio Project: Mixed Messages
 Goal: Generate randomized messages with at least 3 unique components
 */
+
+//Test code
+console.log("Hello, world!");
