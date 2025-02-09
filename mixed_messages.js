c8e035879565e5eca64deb37de39cec45b2854c2
crimar93 <caroline.rimar@gmail.com>
2025-02-08 23:06:45 -0500

    Created function to replace the placeholders in a story

--- mixed_messages.js
+++ mixed_messages.js
@@ -11,5 +11,21 @@ const { toKillAMockingbird, mrRosewater, onTheRoad, chronologyOfWater, dune } = 
 //Functions
 const pickWord = wordList => wordList[Math.floor(Math.random() * wordList.length)]; //Pick a random word from a list
 
+// Function to replace placeholders in a story
+function generateStory(template) {
+    return template.replace(/\{(NOUN|VERB|ADJECTIVE|ADVERB)\}/g, (match, partOfSpeech) => {
+        switch (partOfSpeech) {
+            case "NOUN": return pickWord(nouns);
+            case "VERB": return pickWord(verbs);
+            case "ADJECTIVE": return pickWord(adjectives);
+            case "ADVERB": return pickWord(adverbs);
+            default: return match;
+        }
+    });
+}
+
+
 //Test code
-console.log(pickWord(nouns));
+// Select a random story and generate a filled-in version
+const completedStory = generateStory(toKillAMockingbird);
+console.log(completedStory);
